f2ea7e4bda50ba0bbcc8d6419f958214894b6866
almiraira <131682900+almiraira@users.noreply.github.com>
2023-04-24 19:09:33 +0300

    Добавление функции вывода матрицы на экран

--- ОПИ-лаба №1.cpp
+++ ОПИ-лаба №1.cpp
@@ -75,7 +75,7 @@ void numberOfNonZeroRows(int**& matrix) {
         }
         numberOfZeroElements = 0;
     }
-    cout << "Number of non-zero rows: " << numberOfNonZeroRows << endl; //здесь был вася
+    cout << "Number of non-zero rows: " << numberOfNonZeroRows << endl;
 }
 
 void selectedElements(int**& matrix) {
